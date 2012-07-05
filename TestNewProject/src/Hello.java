3da6d169b01c234e4f42370b100e01caf6b22010
Nirav <nssheth@imagequix.com>
2012-07-05 11:20:21 -0400

    add hello

---
+++ TestNewProject/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author craig
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello12");
+	}
+
+}
